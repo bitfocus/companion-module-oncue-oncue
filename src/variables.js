3d6659921da163387124b13c10646ce0ce1b95d6
Davi Barbosa de Queiroz <daviqueiroz89@gmail.com>
2026-09-25 15:08:26 -0300

    Delete src directory

--- src/variables.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (self) {
-	self.setVariableDefinitions({
-		variable1: { name: 'My first variable' },
-		variable2: { name: 'My second variable' },
-		variable3: { name: 'Another variable' },
-	})
-}
